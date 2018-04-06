950a0f66e1a14ae206facb87fe71f3753e0d8776
Pawel <pawel@lowermycloudbill.com>
2018-04-05 17:52:32 -0700

    increment version

--- install.sh
+++ install.sh
@@ -110,8 +110,8 @@ do_with_root pip install $DEPS
 CLOUDADMIN_FILE_NAME="cloudadmin.conf"
 CLOUDADMIN_CONF_DIR="/etc/cloudadmin/"
 CLOUDADMIN_CONF_URL="https://development-metrics.cloudadmin.io"
-GLANCES_DIR="glances-0.2.1"
-GLANCES_TARBALL_NAME="glances-0.2.1.tar.gz"
+GLANCES_DIR="glances-0.2.2"
+GLANCES_TARBALL_NAME="glances-0.2.2.tar.gz"
 GLANCES_TARBALL_URL="https://s3-us-west-2.amazonaws.com/cloudadmin.io/$GLANCES_TARBALL_NAME"
 
 SYSTEMD_FILE_NAME="glances.service"
