2747a08d1b6d83bec0e71e1a6d6a05b16f59acd2
Pawel <pawel@lowermycloudbill.com>
2018-04-16 22:04:23 -0700

    tested for ubuntu 14.04

--- install.sh
+++ install.sh
@@ -4,36 +4,82 @@
 # Version: Glances 2.8
 # Author:  Nicolas Hennion (aka) Nicolargo
 #
+setup_service() {
+cat > /etc/init.d/glances << 'EOF'
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          Glances
+# Required-Start:    $local_fs $network $named $time $syslog
+# Required-Stop:     $local_fs $network $named $time $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Description:       CloudAdmin fork of Glances
+### END INIT INFO
 
-vercomp () {
-    if [[ $1 == $2 ]]
-    then
-        return 0
-    fi
-    local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
-        ver1[i]=0
-    done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
-            # fill empty fields in ver2 with zeros
-            ver2[i]=0
-        fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
-        then
-            return 1
-        fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
-            return 2
-        fi
-    done
-    return 0
+SCRIPT="/usr/local/bin/glances --quiet --export-http"
+RUNAS=root
+
+PIDFILE=/var/run/glances.pid
+LOGFILE=/var/log/glances.log
+
+start() {
+  if [ -f /var/run/$PIDNAME ] && kill -0 $(cat /var/run/$PIDNAME); then
+    echo 'Service already running' >&2
+    return 1
+  fi
+  echo 'Starting service…' >&2
+  local CMD="$SCRIPT &> \"$LOGFILE\" & echo \$!"
+  su -c "$CMD" $RUNAS > "$PIDFILE"
+  echo 'Service started' >&2
+}
+
+stop() {
+  if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
+    echo 'Service not running' >&2
+    return 1
+  fi
+  echo 'Stopping service…' >&2
+  kill -15 $(cat "$PIDFILE") && rm -f "$PIDFILE"
+  echo 'Service stopped' >&2
+}
+
+uninstall() {
+  echo -n "Are you really sure you want to uninstall this service? That cannot be undone. [yes|No] "
+  local SURE
+  read SURE
+  if [ "$SURE" = "yes" ]; then
+    stop
+    rm -f "$PIDFILE"
+    echo "Notice: log file is not be removed: '$LOGFILE'" >&2
+    update-rc.d -f <NAME> remove
+    rm -fv "$0"
+  fi
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  uninstall)
+    uninstall
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|uninstall}"
+esac
+EOF
+
+#Let's start this up!
+touch /var/log/glances.log
+chmod +x /etc/init.d/glances
+service glances start
+update-rc.d glances default
 }
 
 # Execute a command as root (or sudo)
@@ -130,26 +176,28 @@ else
 fi
 shopt -u nocasematch
 
-echo "Install dependancies"
+# echo "Install dependancies"
 
-# Glances issue #922: Do not install PySensors (SENSORS)
-DEPS="setuptools" 
+# # Glances issue #922: Do not install PySensors (SENSORS)
+# DEPS="setuptools" 
 
-PIP_VERSION=`pip --version | cut -d ' ' -f2`
-vercomp $PIP_VERSION "10.0.0"
+# echo "Hello"
 
-if [ $? == 2 ]
-then
-    do_with_root pip install --upgrade pip --index-url=https://pypi.python.org/simple/
-fi
-do_with_root hash -r
-do_with_root pip install $DEPS
+# PIP_VERSION=`pip --version | cut -d ' ' -f2`
+
+# if [ $PIP_VERSION == "1.0" ]
+# then
+#     # do_with_root pip install --upgrade pip --index-url=https://pypi.python.org/simple/
+# fi
+
+# do_with_root hash -r
+# do_with_root pip install $DEPS
 
 CLOUDADMIN_FILE_NAME="cloudadmin.conf"
 CLOUDADMIN_CONF_DIR="/etc/cloudadmin/"
 CLOUDADMIN_CONF_URL="https://metrics.cloudadmin.io"
-GLANCES_DIR="glances-0.2.5"
-GLANCES_TARBALL_NAME="glances-0.2.5.tar.gz"
+GLANCES_DIR="glances-0.2.7"
+GLANCES_TARBALL_NAME="glances-0.2.7.tar.gz"
 GLANCES_TARBALL_URL="https://s3-us-west-2.amazonaws.com/cloudadmin.io/$GLANCES_TARBALL_NAME"
 
 SYSTEMD_FILE_NAME="glances.service"
@@ -169,8 +217,6 @@ AVAILABILITYZONE=$(curl http://169.254.169.254/latest/meta-data/placement/availa
 #create conf directory for cloudadmin.conf
 do_with_root mkdir -p $CLOUDADMIN_CONF_DIR
 
-echo "HELLOOOOOO!!!"
-
 #dump the config
 cat <<EOF > $CLOUDADMIN_CONF_DIR/$CLOUDADMIN_FILE_NAME
 [CloudAdmin]
@@ -184,87 +230,17 @@ InstanceType=$INSTANCETYPE
 AvailabilityZone=$AVAILABILITYZONE
 EOF
 
-vercomp $UBUNTU_VERSION "12.04"
-
-if [ $? == 0 ]
+if [ "$UBUNTU_VERSION" == "12.04" ]
 then
-cat > /etc/init.d/glances << 'EOF'
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides:          Glances
-# Required-Start:    $local_fs $network $named $time $syslog
-# Required-Stop:     $local_fs $network $named $time $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Description:       CloudAdmin fork of Glances
-### END INIT INFO
+setup_service
+fi
 
-SCRIPT=/usr/local/bin/glances --quiet --export-http
-RUNAS=root
+if [ "$UBUNTU_VERSION" == "14.04" ]
+then
+setup_service
+fi
 
-PIDFILE=/var/run/glances.pid
-LOGFILE=/var/log/glances.log
-
-start() {
-  if [ -f /var/run/$PIDNAME ] && kill -0 $(cat /var/run/$PIDNAME); then
-    echo 'Service already running' >&2
-    return 1
-  fi
-  echo 'Starting service…' >&2
-  local CMD="$SCRIPT &> \"$LOGFILE\" & echo \$!"
-  su -c "$CMD" $RUNAS > "$PIDFILE"
-  echo 'Service started' >&2
-}
-
-stop() {
-  if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
-    echo 'Service not running' >&2
-    return 1
-  fi
-  echo 'Stopping service…' >&2
-  kill -15 $(cat "$PIDFILE") && rm -f "$PIDFILE"
-  echo 'Service stopped' >&2
-}
-
-uninstall() {
-  echo -n "Are you really sure you want to uninstall this service? That cannot be undone. [yes|No] "
-  local SURE
-  read SURE
-  if [ "$SURE" = "yes" ]; then
-    stop
-    rm -f "$PIDFILE"
-    echo "Notice: log file is not be removed: '$LOGFILE'" >&2
-    update-rc.d -f <NAME> remove
-    rm -fv "$0"
-  fi
-}
-
-case "$1" in
-  start)
-    start
-    ;;
-  stop)
-    stop
-    ;;
-  uninstall)
-    uninstall
-    ;;
-  retart)
-    stop
-    start
-    ;;
-  *)
-    echo "Usage: $0 {start|stop|restart|uninstall}"
-esac
-EOF
-
-#Let's start this up!
-touch /var/log/glances.log
-chmod +x /etc/init.d/glances
-update-rc.d glances default
-service glances start
-
-elif [ "$UBUNTU_VERSION" == "14.04" ] || [ "$UBUNTU_VERSION" == "16.04" ] ; then
+if [ "$UBUNTU_VERSION" == "16.04" ] ; then
 cat <<EOF > $SYSTEMD_DIRECTORY/$SYSTEMD_FILE_NAME
 [Unit]
 Description=Glances
